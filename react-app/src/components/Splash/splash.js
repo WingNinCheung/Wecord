18158733ed173441717d077bb4a84aa3220471e4
Krista Strucke <kstrucke@gmail.com>
2022-07-27 09:19:26 -0700

    started a splash page, nbd

---
+++ react-app/src/components/Splash/splash.js
@@ -0,0 +1,13 @@
+import '.splash.css'
+
+import React from 'react'
+
+export default function Splash() {
+  return (
+    <div id="splash-wrapper">
+        <header></header>
+        <body></body>
+        <footer></footer>
+    </div>
+  )
+}
