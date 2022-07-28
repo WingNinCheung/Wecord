35798f1769582bf83f4777333009d6b45ff954a0
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-07-28 13:41:39 -0700

    created footer with each devs

--- react-app/src/components/Splash/splash.js
+++ react-app/src/components/Splash/splash.js
@@ -21,27 +21,90 @@ export default function Splash() {
       </div>
       <div className="footer">
         <section className="dev-info">
-          <div>Ricky Cheung</div>
-          <div>
-            <i class="fa-brands fa-github"></i>
+          <img
+            className="profile-pic"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQGWk2Gqsts5JA/profile-displayphoto-shrink_800_800/0/1563818007698?e=1664409600&v=beta&t=vVd1pvvenK9aSpDCYrZI9CSI6NZHXnAa4b6rjRPlnUk"
+            alt="Ricky"
+          ></img>
+          <div className="profile-section">
+            <div className="name">Ricky Cheung</div>
+
+            <div className="git-in">
+              <a href="https://github.com/WingNinCheung" target="popup">
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wingnincheung/"
+                target="popup"
+              >
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="mailto:wingnin.cheung415@gmail.com">
+                <i class="fa-solid fa-envelope"></i>
+              </a>
+            </div>
           </div>
         </section>
         <section className="dev-info">
-          <div>Krista Strucke</div>
-          <div>
-            <i class="fa-brands fa-github"></i>
+          {/* <img className="profile-pic" src="" alt="Krista"></img> */}
+          <div className="profile-section">
+            <div className="name">Krista Strucke</div>
+
+            <div className="git-in">
+              <a href="" target="popup">
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a href="" target="popup">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="">
+                <i class="fa-solid fa-envelope"></i>
+              </a>
+            </div>
           </div>
         </section>
         <section className="dev-info">
-          <div>Brendan Lau</div>
-          <div>
-            <i class="fa-brands fa-github"></i>
+          {/* <img
+            className="profile-pic"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQGWk2Gqsts5JA/profile-displayphoto-shrink_800_800/0/1563818007698?e=1664409600&v=beta&t=vVd1pvvenK9aSpDCYrZI9CSI6NZHXnAa4b6rjRPlnUk"
+            alt="Ricky"
+          ></img> */}
+          <div className="profile-section">
+            <div className="name">Brendan Lau</div>
+
+            <div className="git-in">
+              <a href="" target="popup">
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a href="" target="popup">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="">
+                <i class="fa-solid fa-envelope"></i>
+              </a>
+            </div>
           </div>
         </section>
         <section className="dev-info">
-          <div>Qiaoyi Liu </div>
-          <div>
-            <i class="fa-brands fa-github"></i>
+          {/* <img
+            className="profile-pic"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQGWk2Gqsts5JA/profile-displayphoto-shrink_800_800/0/1563818007698?e=1664409600&v=beta&t=vVd1pvvenK9aSpDCYrZI9CSI6NZHXnAa4b6rjRPlnUk"
+            alt="Joyce"
+          ></img> */}
+          <div className="profile-section">
+            <div className="name">Qiaoyi Lau</div>
+
+            <div className="git-in">
+              <a href="" target="popup">
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a href="" target="popup">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="">
+                <i class="fa-solid fa-envelope"></i>
+              </a>
+            </div>
           </div>
         </section>
       </div>
