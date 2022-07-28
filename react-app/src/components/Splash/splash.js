db135bef1bc0c8bc5aed6e3be788b36b46af4fa9
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-07-28 12:56:59 -0700

    fixed the background color in splash page

--- react-app/src/components/Splash/splash.js
+++ react-app/src/components/Splash/splash.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './splash.css';
+import React from "react";
+import "./splash.css";
 
 export default function Splash() {
   return (
-    <div id="splash-wrapper">
+    <div className="splash-container">
+      <div id="splash-wrapper">
         <div id="background-partition">
-            <h1 id="splash-title">Imagine a place...</h1>
-            <p id="splash-intro">...where you can belong to any school club, any gaming group, or a worldwide art community.
-            <br/>Where just you and anyone can spend time together.
-            <br/>A place where strangers can become acquaintances...or maybe even friends.</p>
+          <h1 id="splash-title">Imagine a place...</h1>
+          <p id="splash-intro">
+            ...where you can belong to any school club, any gaming group, or a
+            worldwide art community.
+            <div></div>
+            <br />
+            Where just you and anyone can spend time together.
+            <div></div>
+            <br />A place where strangers can become acquaintances...or maybe
+            even friends.
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
