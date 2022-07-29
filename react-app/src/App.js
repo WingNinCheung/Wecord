ef1ad23e26fbc444bc1cd9b48453623d97dc0373
Krista Strucke <kstrucke@gmail.com>
2022-07-29 11:32:46 -0700

    fixed class tags, added some default styling, public servers styling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,7 @@ import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/ServerList";
+import UsersList from "./components/HomePage/PublicServers/ServerList";
 import User from "./components/User";
 import HomePage from "./components/HomePage";
 import { authenticate } from "./store/session";
